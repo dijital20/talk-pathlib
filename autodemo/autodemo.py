8f854253971d3a513b0871a40fde3f45a333e703
Josh Schneider <josh.schneider@gmail.com>
2024-06-21 13:51:45 -0500

    Now with multiline support and color!

--- autodemo/autodemo.py
+++ autodemo/autodemo.py
@@ -1,11 +1,71 @@
 """Automating demo."""
+
 import argparse
 from collections.abc import Iterator
 import os
 import shutil
 import traceback as tb
 from pathlib import Path
-from typing import TextIO
+from typing import Any, TextIO
+
+
+class Ansi:
+    black = "\u001b[30m"
+    red = "\u001b[31m"
+    green = "\u001b[32m"
+    yellow = "\u001b[33m"
+    blue = "\u001b[34m"
+    magenta = "\u001b[35m"
+    cyan = "\u001b[36m"
+    white = "\u001b[37m"
+
+    bright_black = "\u001b[30;1m"
+    bright_red = "\u001b[31;1m"
+    bright_green = "\u001b[32;1m"
+    bright_yellow = "\u001b[33;1m"
+    bright_blue = "\u001b[34;1m"
+    bright_magenta = "\u001b[35;1m"
+    bright_cyan = "\u001b[36;1m"
+    bright_white = "\u001b[37;1m"
+
+    back_black = "\u001b[40m"
+    back_red = "\u001b[41m"
+    back_green = "\u001b[42m"
+    back_yellow = "\u001b[43m"
+    back_blue = "\u001b[44m"
+    back_magenta = "\u001b[45m"
+    back_cyan = "\u001b[46m"
+    back_white = "\u001b[47m"
+
+    back_bright_black = "\u001b[40;1m"
+    back_bright_red = "\u001b[41;1m"
+    back_bright_green = "\u001b[42;1m"
+    back_bright_yellow = "\u001b[43;1m"
+    back_bright_blue = "\u001b[44;1m"
+    back_bright_magenta = "\u001b[45;1m"
+    back_bright_cyan = "\u001b[46;1m"
+    back_bright_white = "\u001b[47;1m"
+
+    reset = "\u001b[0m"
+
+    cursor_up = "\u001b[1A"
+    cursor_down = "\u001b[1B"
+    cursor_right = "\u001b[1C"
+    cursor_left = "\u001b[1D"
+
+    clear_screen_cursor_to_end = "\u001b[0J"
+    clear_screen_cursor_to_begin = "\u001b[1J"
+    clear_screen = "\u001b[2J"
+
+    clear_line_cursor_to_begin = "\u001b[0K"
+    clear_line_cursor_to_end = "\u001b[1K"
+    clear_line = "\u001b[2K"
+
+    cursor_pos_save = "\u001b[s"
+    cursor_pos_restore = "\u001b[u"
+
+
+PROMPT = f"{Ansi.green}(q for quit, any other key to continue):{Ansi.reset} "
 
 
 def _canonical_path(val: str) -> Path:
@@ -13,9 +73,48 @@ def _canonical_path(val: str) -> Path:
     return Path(val).resolve()
 
 
-def _get_width() -> int:
-    """Get the width of the terminal."""
-    return shutil.get_terminal_size()[0]
+def _find_executable_lines(f: TextIO) -> Iterator[str]:
+    """Perform a buffered read through the file until we run out of content.
+
+    Args:
+        f: File object.
+
+    Yields:
+        Iterator[str]: Each executable line.
+    """
+    buffer = f.read(1)
+    while True:
+        if not (char := f.read(1)):
+            yield buffer.strip()
+            break
+
+        buffer += char
+        if buffer[-2] == "\n" and buffer[-1] not in " \t\n\r":
+            yield buffer[:-2].strip()
+            buffer = buffer[-1]
+
+
+def _write_local_scope(local_scope: dict[str, Any]) -> int:
+    """Write the local scope to the console.
+
+    Args:
+        local_scope: Local scope dictionary.
+
+    Returns:
+        Length of the message printed.
+    """
+    width = shutil.get_terminal_size()[0]
+    if local_scope:
+        print(f"\n{Ansi.bright_blue}Locals:{Ansi.reset}")
+        for name, value in local_scope.items():
+            value_width = width - len(name) - 3
+            value_repr = repr(value)
+            value = (
+                value_repr
+                if len(value_repr) < value_width
+                else f"{value_repr[:value_width - 12]}...{value_repr[-8:]}"
+            )
+            print(f"{Ansi.red}{name}{Ansi.reset} = {value}")
 
 
 def process_file(path: Path):
@@ -26,32 +125,38 @@ def process_file(path: Path):
     """
     with path.open(mode="r", encoding="UTF-8") as f:
         local_scope = {}
-        for idx, expression in enumerate(f, start=1):
-            print(f"[{idx:>3}] >>> {expression.rstrip()}")
+        for expression in _find_executable_lines(f):
+            print(
+                f"{Ansi.bright_magenta}>>>{Ansi.reset} "
+                + f"\n{Ansi.bright_magenta}...{Ansi.reset} ".join(
+                    expression.split("\n")
+                )
+            )
 
             try:
                 result = eval(expression, globals(), local_scope)
             except SyntaxError:
-                exec(expression, globals(), local_scope)
+                result = exec(expression, globals(), local_scope)
             except Exception as e:
                 tb.print_exception(e)
             else:
                 if result is not None:
                     print(f"{result!r}")
 
-            if local_scope:
-                print(
-                    "\nLocals:\n"
-                    f"{"\n".join(f"  {k} = {v!r}" for k, v in local_scope.items())}",
-                )
+            _write_local_scope(local_scope)
 
-            if (
-                input("(q for quit, any other key to continue): ").strip().lower()
-                == "q"
-            ):
+            if input(PROMPT).strip().lower() == "q":
                 break
 
-            print(f'\033[A\r{' ' * _get_width()}\r')
+            print(
+                (
+                    Ansi.cursor_up * (len(local_scope) + 3)
+                    + Ansi.clear_screen_cursor_to_end
+                ),
+                end="",
+            )
+
+    _write_local_scope(local_scope)
 
 
 if __name__ == "__main__":
